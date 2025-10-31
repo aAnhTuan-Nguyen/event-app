e00a4bff8de89eca33e079ffea083c0bf78eb904
Nguyen Anh Tuan <nguyenanhtuandl1504@gmail.com>
2025-11-01 00:21:24 +0700

    Integrate PostHog for analytics and error tracking

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,26 @@
-import type { NextConfig } from "next"
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
   // từ react 18 hay nextjs 16 nó có hổi trợ cái babel mới để biên dịch react nhanh hơn
   // và nó reduce render những cái không cần thiết
   reactCompiler: true,
+
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://us-assets.i.posthog.com/static/:path*'
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://us.i.posthog.com/:path*'
+      }
+    ]
+  },
+
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true
 }
 
 export default nextConfig
