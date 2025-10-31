67e5a0ce1b86152716957ace641246a381b11a8c
Nguyen Anh Tuan <nguyenanhtuandl1504@gmail.com>
2025-10-31 17:08:09 +0700

    finish stepup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
+import ExploreButton from '@/components/ExploreButton'
+
 const page = () => {
-  console.log('This is a servercomponent')
-  return <div>page</div>
+  return (
+    <section>
+      <h1 className='text-center'>
+        The Hub for every Developer <br /> Event you can not miss
+      </h1>
+      <p className='text-center mt-5'>Join us for the latest updates and events in the developer community.</p>
+
+      <ExploreButton />
+    </section>
+  )
 }
 
 export default page
