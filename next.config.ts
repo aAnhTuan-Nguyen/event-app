052d461aa13228b161c01ccf837288d4d4ef4463
Nguyen Anh Tuan <nguyenanhtuandl1504@gmail.com>
2025-10-31 13:41:15 +0700

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  // từ react 18 hay nextjs 16 nó có hổi trợ cái babel mới để biên dịch react nhanh hơn
+  // và nó reduce render những cái không cần thiết
+  reactCompiler: true,
+}
 
-export default nextConfig;
+export default nextConfig
